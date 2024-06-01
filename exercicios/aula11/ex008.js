924cbfd12af524db95f4f6a8a5e2de14f2540eb1
paulohjr <pauloh.jr@hotmail.com>
2024-06-01 18:50:37 -0300

    att

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 91
+console.log(`A Velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){ //condição simples
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}
---
+++ exercicios/aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 54
+console.log(`Você tem ${idade} anos`)
+if (idade < 16){
+    console.log('Não vota')
+        }else if ( idade < 18 || idade >= 67){ 
+            console.log('Vota Opcional') 
+        }else if (idade >= 18){ 
+            console.log('Vota')
+        }
+        
